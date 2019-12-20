88029b89bc0860f7d80f7ae11b06b3f91139e0a0
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2019-12-20 14:31:45 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OperatorLogika__CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Umur = 25;
+            double Uang = 900;
+            bool Kawin = false;
+
+            if (!(Umur > 25) || (Uang > 1000) && !Kawin) 
+            {
+                Console.WriteLine("Sukses");
+            }
+            else
+            {
+                Console.WriteLine("Gagal");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
